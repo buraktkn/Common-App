4cb614d404687b348e3674d1efcadfa1320a5a0f
unknown <burak.tekin@std.medipol.edu.tr>
2022-07-22 16:02:34 +0300

    Navbar Created

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <a href="www.google.com">Home</a>
+      <a href="www.google.com">Menu</a>
+      <a href="www.google.com">Contact</a>
+    </div>
+  )
+}
